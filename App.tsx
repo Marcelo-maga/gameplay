aa10d9faa92b7578bd6fef611289c614e34b5326
Marcelo Magalhães <marcelo.maga2550@gmail.com>
2021-09-19 18:12:26 -0300

    Efeito de degrade e instalação das fontes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,34 @@
-import React from "react";
-import { SingIn } from "./src/screens/SingIn";
+import React from "react"
+import { useFonts } from 'expo-font'
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
+import AppLoading from "expo-app-loading"
+
+import { SingIn } from "./src/screens/SingIn"
+import { StatusBar } from "react-native"
+import { Background } from "./src/components/Background"
 
 export default function App(){
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium,
+
+    Rajdhani_500Medium, 
+    Rajdhani_700Bold
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
   return(
-    <SingIn/>
+    <Background>
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='trasparent'
+        translucent
+      />
+      <SingIn/>
+    </Background>
   )
 }
